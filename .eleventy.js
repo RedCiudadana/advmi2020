8b2adb15d263b47e8a1ff4d8568dcbadbf5620d8
Diego Auyón <diegoeauyonh@gmail.com>
2021-01-11 12:31:20 -0600

    Agregar roadmap

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const pluginSass = require("eleventy-plugin-sass");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./css");
     eleventyConfig.addPassthroughCopy("./fonts");
@@ -6,5 +8,9 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addFilter('toLocaleString', function (value) {
         return parseFloat(value).toLocaleString();
+    });
+
+    eleventyConfig.addPlugin(pluginSass, {
+        outputDir: './_site/'
     });
 };
